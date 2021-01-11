fb997afca79d9eb27bb42c91ea6144961d2181be
AALEKH1111 <72079790+AALEKH1111@users.noreply.github.com>
2021-01-11 19:09:43 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,57 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playercountref=await database.ref("playerCount").once('value');
+      if (playercountref.exists())
+      {
+       playerCount=playercountref.val()
+       player.getCount();
+      }
+      form = new Form();
+      form.display();
+    }
+  }
+  play()
+{
+  form.hide();
+  textSize(30);
+  text("game start",120,100);
+  Player.getplayerinfo();
+  if(allplayers!==undefined)
+  {
+    var displayposition=150;
+    for(var plr in allplayers)
+    {
+      if(plr==="player"+player.index)
+      fill("red");
+      else
+      fill("black");
+      displayposition=displayposition+20;
+      textSize(15);
+      text(allplayers[plr].name+":"+allplayers[plr].distance,120,displayposition);
+    }
+  }
+  if(keyisdown(UP_ARROW)&&player.index!==null)
+  {
+    player.distance+=50
+    player.update();
+  }
+}
+}
